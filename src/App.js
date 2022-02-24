ac3c66bbc05c9e70cd2185fe4b93864cf14c4e6e
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-24 23:56:38 +0800

    Update App.js
    This mini test was successful.
    It's time to delete these lines.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React from "react"
 function App() {
   return (
     <div className="App">
-      <h1>Testing the app...</h1>
-      <h2>Seems to be working fine so far.</h2>
+      
     </div>
   );
 }
