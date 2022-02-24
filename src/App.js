0d9e32080c059d897f7d29194b4f84603bb9bf4a
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-24 23:55:12 +0800

    Update App.js
    Testing the functionality of the React app.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from "react"
 function App() {
   return (
     <div className="App">
-    
+      <h1>Testing the app...</h1>
+      <h2>Seems to be working fine so far.</h2>
     </div>
   );
 }
