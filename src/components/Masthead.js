69dfd6f9f803042fa1cca2cb65350ac98e406fd3
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-25 21:29:51 +0800

    Update Masthead.js
    The masthead is planned to be light green only in the homepage and sign up page.
    
    Changing the header class name from "mastHead-green" to "mastHead" and creating the id "green" provide flexibility.

--- src/components/Masthead.js
+++ src/components/Masthead.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Masthead() {
   return (
     <div>
-      <header className="mastHead-green">
+      <header className="mastHead" id="green">
         <h1 className="siteName">DaftarCOV</h1>
       </header>
     </div>
