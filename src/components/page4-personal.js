20473926b0d0c85cfb1aaf91ad79c1029f4df823
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-25 23:15:13 +0800

    Profile and Full Details Pages
    Applying the masthead and sign out button in these pages.

---
+++ src/components/page4-personal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Masthead from './masthead-blue'
+import SignOut from './sign-out'
+
+export default function fullPersonalDetails() {
+  return (
+    <div>
+        <Masthead />
+        <SignOut />
+    </div>
+  )
+}
---
+++ src/components/page5-vaccine.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Masthead from './masthead-blue'
+import SignOut from './sign-out'
+
+export default function fullAppointmentDetails() {
+  return (
+    <div>
+        <Masthead />
+        <SignOut />
+    </div>
+  )
+}
