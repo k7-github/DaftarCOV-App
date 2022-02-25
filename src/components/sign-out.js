65c5fede083277f86711b72fbef80b8f7a0e3225
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-25 22:34:07 +0800

    Create sign-out.js
    This is the Sign Out button component.

---
+++ src/components/sign-out.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function signOut() {
+  return (
+    <div>
+        <h3 class="signOut">SIGN OUT</h3>
+        {/* This is the button users click if they want to sign out. */}
+    </div>
+  )
+}
