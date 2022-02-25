b575682409c25addae858e81df177ba6f6f7d61f
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-25 23:06:52 +0800

    Update page2-sign-up.js
    This is the Vaccine Registration Page.
    The forms in sign-up.js have not been implemented.

---
+++ src/components/page2-sign-up.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Masthead from './masthead-green'
+import SignUp from './sign-up'
+
+export default function signUpPage () {
+  return (
+    <div>
+        <Masthead />
+        <SignUp/>
+    </div>
+  )
+}
