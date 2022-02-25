4f70cf49c55216578f4db01b8145536f4bce3423
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-25 22:51:32 +0800

    Update App.js
    The deleted lines are no longer necessary.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react"
-import Masthead1 from "./components/masthead-green";
-import Masthead2 from "./components/masthead-blue";
+import Homepage from './components/page1-home'
 
 function App() {
   return (
     <div className="App">
-      {/* <Masthead1 /> */}
-      <Masthead2 />
+      <Homepage />
     </div>
   );
 }
