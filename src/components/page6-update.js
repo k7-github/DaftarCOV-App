11d427b01ed82ddc7714d3718f80957b565493fc
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-25 23:07:17 +0800

    Update page6-update.js
    This is the Account Update Page.
    The forms in update.js have not been implemented.

---
+++ src/components/page6-update.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Masthead from './masthead-blue'
+import SignOut from './sign-out'
+import Update from './update'
+
+export default function updatePage() {
+  return (
+    <div>
+        <Masthead />
+        <SignOut />
+        <Update />
+    </div>
+  )
+}
