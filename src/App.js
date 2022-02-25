109a1980b5bd279fd99ec1e0ebc2b54859d30e88
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-25 23:39:31 +0800

    Update App.js
    Importing all the pages in the web app.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react"
 import Homepage from './components/page1-home'
+import SignUpPage from './components/page2-sign-up'
+import ProfilePage from './components/page3-profile'
+import PersonalPage from './components/page4-personal'
+import VaccinePage from './components/page5-vaccine'
+import UpdatePage from './components/page6-update'
 
 function App() {
   return (
     <div className="App">
       <Homepage />
+      <SignUpPage />
+      <ProfilePage />
+      <PersonalPage />
+      <VaccinePage />
+      <UpdatePage />
+
     </div>
   );
 }
