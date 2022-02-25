431ad655de88e825480d8f39c1f732930980a6ce
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-25 22:52:11 +0800

    Update page1-home.js
    This is the landing page.
    The Admin Sign In button is removed.

---
+++ src/components/page1-home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Masthead from './masthead-green'
+
+export default function homepage() {
+  return (
+    <div>
+      <Masthead />
+      <div className="landingPage">
+      {/* This is the main section of the homepage. */}
+        <div className="Welcome">
+            <h2>Welcome to DaftarCOV! You Can Register for the Malaysian National COVID-19 Immunisation Programme Here!</h2>
+        </div>
+        <nav className= "signUpInButtonSection">
+            <button className="signUpInButtons">Vaccine Register</button>
+            {/* The Vaccination Registration page will load when this button is clicked. */}
+            <button className="signUpInButtons">User Sign In</button>
+            {/* When this button is clicked, the website will load a prompt window. */}
+            {/* The prompt window requests the user's ID and password. */}
+            {/* If the user inputs both correctly, the website will load the user's profile page. */}
+            {/* Otherwise, the website displays one of 3 dialog boxes: */}
+                {/* "Sorry, we cannot find an account with this user ID. Please try again." */}
+                {/* "Sorry, we cannot find an account with this password. Please try again."     */}
+                {/* "Sorry, we cannot find an account with this user ID and password. Please try again." */}
+        </nav>
+    </div>
+    </div>
+  )
+}
