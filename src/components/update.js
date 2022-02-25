65b25bc74cab35dba41093fbf903959e6d4876ac
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-25 22:39:39 +0800

    Create update.js
    Lines 6 and 7 are placeholders.

---
+++ src/components/update.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function accountUpdate() {
+  return (
+    <div>
+        <h1>ACCOUNT UPDATE PAGE</h1>
+        <h3>THIS IS A PLACEHOLDER</h3>
+    </div>
+  )
+}
