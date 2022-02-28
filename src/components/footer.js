abd3251e770e2c33df155ee431b868962611ae6b
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-28 18:37:24 +0800

    Create footer.js
    This footer appears at the bottom of each page.

---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <small className='footer'>A Final Year Project by Kesavan Osbourne Satkunananthan from Open University Malaysia. @2022</small>
+  )
+}
