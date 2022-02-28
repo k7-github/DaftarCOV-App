a3f33a04dea0c7a00f22b180561d7caffaec5fef
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-28 23:14:29 +0800

    Update App.js
    Removed ./components/page3-profile, the correct file.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react"
 import Homepage from './components/page1-home'
 import SignUpPage from './components/page2-sign-up'
-import ProfilePage from './components/page3-profile'
-import PersonalPage from './components/page4-personal'
 import VaccinePage from './components/page5-vaccine'
+import PersonalPage from './components/page4-personal'
 import UpdatePage from './components/page6-update'
 
 function App() {
@@ -11,9 +10,8 @@ function App() {
     <div className="App">
       <Homepage />
       <SignUpPage />
-      <ProfilePage />
-      <PersonalPage />
       <VaccinePage />
+      <PersonalPage />
       <UpdatePage />
 
     </div>
