cc5a0e5fbc67f3537a0facc06fe586a845f628dc
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-28 16:48:30 +0800

    Update page6-update.js
    Removed update.js from this file because it is deemed to be no longer necessary.

--- src/components/page6-update.js
+++ src/components/page6-update.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Masthead from './masthead-blue'
-import Update from './update'
 
 export default function updatePage() {
   return (
     <div>
         <Masthead />
-        <Update />
     </div>
   )
 }
