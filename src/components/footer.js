46e6537764ae733eec1d96444f6402a65a45a190
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-28 22:44:42 +0800

    Update footer.js
    Renaming the class name of the element at Line 6 to 'copyright'.
    
    That element is placed inside an actual footer element.

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <small className='footer'>A Final Year Project by Kesavan Osbourne Satkunananthan from Open University Malaysia. @2022</small>
+    <footer>
+      <small className='copyright'>A Final Year Project by Kesavan Osbourne Satkunananthan from Open University Malaysia. @2022</small>
+    </footer>
   )
 }
