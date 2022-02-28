a07f33d33b5c0ea39ce73138c97bbd9dd94ebfee
k7-github <100348682+k7-github@users.noreply.github.com>
2022-02-28 23:12:25 +0800

    Update App.js
    I realized that I removed the wrong file.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from "react"
 import Homepage from './components/page1-home'
 import SignUpPage from './components/page2-sign-up'
 import ProfilePage from './components/page3-profile'
+import PersonalPage from './components/page4-personal'
 import VaccinePage from './components/page5-vaccine'
 import UpdatePage from './components/page6-update'
 
 function App() {
   return (
     <div className="App">
-      {/* <Homepage /> */}
-      {/* <SignUpPage /> */}
-      {/* <ProfilePage /> */}
-      {/* <VaccinePage /> */}
+      <Homepage />
+      <SignUpPage />
+      <ProfilePage />
+      <PersonalPage />
+      <VaccinePage />
       <UpdatePage />
 
     </div>
