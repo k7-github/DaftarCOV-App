d0f9d2c8eff1773c7513785debd60d5e7705b4c6
k7-github <100348682+k7-github@users.noreply.github.com>
2022-03-01 22:31:08 +0800

    Delete 1 File and Rename 2 More.
    page3-profile.js is deemed to be no longer necessary.
    
    Therefore, the following 2 files have to be renamed.
    
    page5-vaccine.js to page3-vaccine.js.
    page6-update.js to page5-update.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react"
 import Homepage from './components/page1-home'
 import SignUpPage from './components/page2-sign-up'
-import VaccinePage from './components/page5-vaccine'
+import VaccinePage from './components/page3-vaccine'
 import PersonalPage from './components/page4-personal'
-import UpdatePage from './components/page6-update'
+import UpdatePage from './components/page5-update'
 
 function App() {
   return (
