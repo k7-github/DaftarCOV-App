9bb76898d99688abcc081d7e7d9ac25261197bce
k7-github <100348682+k7-github@users.noreply.github.com>
2022-03-01 23:24:23 +0800

    Update App.js
    Applying React Router.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { BrowserRouter, Route } from "react-router-dom"
 import Homepage from './components/page1-home'
 import SignUpPage from './components/page2-sign-up'
 import VaccinePage from './components/page3-vaccine'
@@ -7,13 +8,20 @@ import UpdatePage from './components/page5-update'
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
-      <SignUpPage />
-      <VaccinePage />
-      <PersonalPage />
-      <UpdatePage />
+    <BrowserRouter>
+      <div className="App">
+        {/* <Homepage />
+        <SignUpPage />
+        <VaccinePage />
+        <PersonalPage />
+        <UpdatePage /> */}
+        <Route path='/'         component={Homepage} />
+        <Route path='/signup'   component={SignUpPage} />
+        <Route path='/vaccine'  component={VaccinePage} />
+        <Route path='/personal' component={PersonalPage} />
+        <Route path='/update'   component={UpdatePage} />
     </div>
+    </BrowserRouter>
   );
 }
 
