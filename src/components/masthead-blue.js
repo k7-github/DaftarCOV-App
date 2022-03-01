d34ed4e924bd7649ff22a036ac4709964b8bdef8
k7-github <100348682+k7-github@users.noreply.github.com>
2022-03-01 20:54:10 +0800

    Update masthead-blue.js
    Imported useState from React.
    
    Renamed the function mastheadBlue() to MastheadBlue() because "React component names must start with an uppercase letter."
    
    Created the variable [login, setLogin] for the React Hook.
    
    Added button functionality to the 'Sign Out' element.

--- src/components/masthead-blue.js
+++ src/components/masthead-blue.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
 
 // The masthead appears at the top of every page in this website.
 // It is light green in the homepage and Vaccine Registration page.
 // It is light blue in the other pages.
 
-export default function mastheadBlue() {
+export default function MastheadBlue() {
+
+  const [login, setlogin] = useState(true)
+
+  function signOut() {
+    setlogin(false)
+    console.log(login)
+  }
+
   return (
     <div>
       <header className="mastHead" id="blue">
         <h1 className="siteName">DaftarCOV</h1>
-        <h3 className="signOut">SIGN OUT</h3>
+        <h3 className="signOut" onClick={signOut}>SIGN OUT</h3>
         {/* This is the button users click if they want to sign out. */}
       </header>
     </div>
