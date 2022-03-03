ac00a5a3a5aa0cb285eaaafa05112784a6696340
k7-github <100348682+k7-github@users.noreply.github.com>
2022-03-03 23:45:54 +0800

    Update App.js
    Lines 2, 18 - 24
    Rewriting the React Router code.
    
    Lines 3 - 7
    The page files have been moved out of the components folder and to the newly created pages folder.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react"
-import { BrowserRouter, Route } from "react-router-dom"
-import Homepage from './components/page1-home'
-import SignUpPage from './components/page2-sign-up'
-import VaccinePage from './components/page3-vaccine'
-import PersonalPage from './components/page4-personal'
-import UpdatePage from './components/page5-update'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Homepage from './pages/page1-home'
+import SignUpPage from './pages/page2-sign-up'
+import VaccinePage from './pages/page3-vaccine'
+import PersonalPage from './pages/page4-personal'
+import UpdatePage from './pages/page5-update'
 
 function App() {
   return (
@@ -15,13 +15,15 @@ function App() {
     //     <PersonalPage />
     //     <UpdatePage />
     // </div>
-    <BrowserRouter basename="/daftarcov">
-        <Route path='/'         component={Homepage} />
-        <Route path='/signup'   component={SignUpPage} />
-        <Route path='/vaccine'  component={VaccinePage} />
-        <Route path='/personal' component={PersonalPage} />
-        <Route path='/update'   component={UpdatePage} />
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path='/'         element={<Homepage />} />
+        <Route path='/signup'   element={<SignUpPage />} />
+        <Route path='/vaccine'  element={<VaccinePage />} />
+        <Route path='/personal' element={<PersonalPage />} />
+        <Route path='/update'   element={<UpdatePage />} />
+      </Routes>
+    </Router>
   );
 }
 
