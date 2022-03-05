af2e99cf007fc4593e0e3dfeb35a6ee8d8624cea
k7-github <100348682+k7-github@users.noreply.github.com>
2022-03-06 00:17:43 +0800

    Update masthead-green.js
    Line 2:
    Importing useNavigate.
    
    Line 8: React components must start with an uppercase letter.
    
    Line 13: Adding button functionality.

--- src/components/masthead-green.js
+++ src/components/masthead-green.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 // The masthead appears at the top of every page in this website.
 // It is light green in the homepage and Vaccine Registration page.
 // It is light blue in the other pages.
 
-export default function mastheadGreen() {
+export default function MastheadGreen() {
+  const navigate = useNavigate()
   return (
     <div>
       <header className="mastHead" id="green">
-        <h1 className="siteName">DaftarCOV</h1>
+        <h1 className="siteName" onClick={() => {navigate('/')}}>DaftarCOV</h1>
       </header>
     </div>
   )
