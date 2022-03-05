d24adca529a8df53c63b39e30e59df7e5764e056
k7-github <100348682+k7-github@users.noreply.github.com>
2022-03-06 00:21:13 +0800

    Update 1_home.js
    Lines 2, 9:
    Re-inserting Masthead.
    
    Lines 3, 6, 16:
    Adding button functionality to Vaccine Register.

--- src/pages/1_home.js
+++ src/pages/1_home.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import Masthead from '../components/masthead-green'
+import { useNavigate } from 'react-router-dom'
 
 export default function Homepage() {
+  const navigate = useNavigate()
   return (
     <div>
+      <Masthead />
       <div className="landingPage">
       {/* This is the main section of the homepage. */}
         <div className="Welcome" id='green'>
             <h2>Welcome to DaftarCOV! You Can Register for the Malaysian National COVID-19 Immunisation Programme Here!</h2>
         </div>
         <nav className= "signUpInButtonSection">
-            <button className="signUpInButtons">Vaccine Register</button>
+            <button className="signUpInButtons" onClick={() => {navigate('/signup')}}>Vaccine Register</button>
             {/* The Vaccination Registration page will load when this button is clicked. */}
             <button className="signUpInButtons">User Sign In</button>
             {/* When this button is clicked, the website will load a prompt window. */}
