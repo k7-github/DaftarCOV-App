dff689b89cb4b8bb2fd8b3ff0b0dd1d9de3d7cf3
k7-github <100348682+k7-github@users.noreply.github.com>
2022-03-11 01:08:57 +0800

    Update App.js
    Line 1
    Imported useState from React
    
    Line 12
    Added the state signedIn
    
    Lines 17 - 21
    Added conditions to the routes.
    Passed the function setSignedIn in the routes through props.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Footer from "./components/footer"
 import Homepage from './pages/1_home'
@@ -8,14 +8,17 @@ import PersonalPage from './pages/4_personal'
 import UpdatePage from './pages/5_update'
 
 function App() {
+  
+  const [signedIn, setSignedIn] = useState(false)
+  
   return (
     <Router>
       <Routes>
-        <Route path='/'         element={<Homepage />} />
-        <Route path='/signup'   element={<SignUpPage />} />
-        <Route path='/vaccine'  element={<VaccinePage />} />
-        <Route path='/personal' element={<PersonalPage />} />
-        <Route path='/update'   element={<UpdatePage />} />
+        { signedIn === false && <Route path='/'         element={<Homepage />} setSignedIn={setSignedIn} />}
+        { signedIn === false && <Route path='/signup'   element={<SignUpPage />} setSignedIn={setSignedIn} />}
+        { signedIn === true && <Route path='/vaccine'  element={<VaccinePage />} setSignedIn={setSignedIn} />}
+        { signedIn === true && <Route path='/personal' element={<PersonalPage />} setSignedIn={setSignedIn} />}
+        { signedIn === true && <Route path='/update'   element={<UpdatePage />} setSignedIn={setSignedIn} />}
       </Routes>
       <Footer />
     </Router>
