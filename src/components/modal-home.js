c3d138c4096c9b8b0b154aae21dc1fd1cb209af6
k7-github <100348682+k7-github@users.noreply.github.com>
2022-03-11 20:54:28 +0800

    Update modal-home.js
    Line 2
    Imported useNavigate Hook from React Router.
    
    Line 5
    Added the constant  navigate = useNavigate().
    
    Lines 6 - 8
    Used the above constant to define the function toPersonal.
    
    Line 24
    Added the function toPersonal to the Sign In button.

--- src/components/modal-home.js
+++ src/components/modal-home.js
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 export default function ModalHome({setModal}) {
-  
+    
+    const navigate = useNavigate()
+    function toPersonal () {
+        navigate('/personal')
+      }
+    
     function cancelSignIn() {
         setModal(false)
     }
@@ -16,7 +21,7 @@ export default function ModalHome({setModal}) {
                 <p>No need to enter credentials now</p>
             </div>
             <div className='modal-bottom'>
-                <button className='modal-buttons'>Sign In</button>
+                <button className='modal-buttons' onClick={toPersonal}>Sign In</button>
                 <button className='modal-buttons' onClick={cancelSignIn}>Cancel</button>
             </div>
         </div>
