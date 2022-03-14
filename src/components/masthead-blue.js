0a76c920b8524d7c43562cdfc68433b07c59aaea
k7-github <100348682+k7-github@users.noreply.github.com>
2022-03-14 18:47:13 +0800

    Update masthead-blue.js
    Line 1
    Imported useState from React
    
    Line 3
    Imported the React component SignOutModal.
    
    Line 9
    The prop setSignedIn is no longer necessary as this state setter is implemented in SignOutModal through useContext.
    
    Line 13
    Defined the state constants modal and setModal for the SignOutModal component.
    
    Red Lines 18 - 20
    These lines were moved to the SignOutModal component.
    
    Lines 21 - 22
    The function signOutModal() (not to be mistaken with the similarly named component) changes the state of the modal state to true.
    
    Lines 27 and 29
    Replaved the function signOut() attached to the "SIGN OUT" button with the signOutModal () function.
    
    Line 32
    Set React conditionals for the SignOutModal component.
    The component will appear only when the modal state is true, which happens only when signOutModal() function is executed.

--- src/components/masthead-blue.js
+++ src/components/masthead-blue.js
@@ -1,32 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import SignOutModal from './modal-sign-out'
 
 // The masthead appears at the top of every page in this website.
 // It is light green in the homepage and Vaccine Registration page.
 // It is light blue in the other pages.
 
-export default function MastheadBlue({setSignedIn}) {
+export default function MastheadBlue() {
 
   // Add function to DaftarCOV element in the masthead.
   // When this element is clicked, the website will load the Full Personal Page of the signed user.
+  const [modal, setModal] = useState(false)
+  
   const navigate = useNavigate()
   
   function toPersonal () {
     navigate('/personal')
   }
   
-  function signOut() {
-    setSignedIn(false)
-    navigate('/')
+  function signOutModal() {
+    setModal(true)
   }
 
   return (
     <div>
       <header className="mastHead" id="blue">
         <h1 className="siteName" onClick={toPersonal}>DaftarCOV</h1>
-        <h3 className="signOut" onClick={signOut}>SIGN OUT</h3>
+        <h3 className="signOut" onClick={signOutModal}>SIGN OUT</h3>
         {/* This is the button users click if they want to sign out. */}
       </header>
+      {modal && <SignOutModal setModal={setModal} />}
     </div>
   )
 }
