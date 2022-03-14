613409f1399fee2a6ae09c9de093b8aa12873e4d
k7-github <100348682+k7-github@users.noreply.github.com>
2022-03-14 18:52:51 +0800

    Update modal-home.js
    Line 1
    Imported useContext from React.
    
    Line 2
    Imported LoginContext from the login-context.js file.
    
    Lines 3 and 5
    Removed the setSignedIn prop due the implementation of useContext and LoginContext.
    
    Line 7
    Defined the constant setSignedIn using useContext and LoginContext.
    
    Lines 22, 25 and 26
    Replaced the text at Line 22 with
    the text at Lines 25 and 26.
    The text at these lines are placeholders for web forms which will be implemented in the future.

--- src/components/modal-home.js
+++ src/components/modal-home.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { LoginContext } from './login-context'
 import { useNavigate } from 'react-router-dom'
-export default function ModalHome({setModal, setSignedIn}) {
+
+export default function ModalHome({setModal}) {
     
+    const { setSignedIn } = useContext(LoginContext)
     const navigate = useNavigate()
     function toPersonal () {
         setSignedIn(true)
@@ -19,7 +22,8 @@ export default function ModalHome({setModal, setSignedIn}) {
                 <p>User Sign In</p>
             </div>
             <div className='modal-body'>
-                <p>No need to enter credentials now</p>
+                <p>(Web form for user's email address)</p>
+                <p>(Web form for user's password)</p>
             </div>
             <div className='modal-bottom'>
                 <button className='modal-buttons' onClick={toPersonal}>Sign In</button>
