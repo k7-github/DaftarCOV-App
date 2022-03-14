1f75f92edf044395cb55c3c9c6176983b9960c7f
k7-github <100348682+k7-github@users.noreply.github.com>
2022-03-14 17:43:56 +0800

    Create modal-sign-out.js
    Lines 1 - 3
    Imported React, useContext, LoginContext and useNavigate.
    
    Lines 5 - 33
    Created React component SignOutModal which appears when user clicks the Sign Out button.
    
    Lines 6 - 7
    Defined the following constants using useContext and useNavigate respectively.
    
    Lines 9 - 16
    Created the functions cancelSignOut() and SignOut() .
    the first function closes the SignOutModal component when executed.
    The second function logs out the user.
    
    Lines 17 - 32
    Returned the modal window and overlay.
    The "Yes" button has the signOut() function, whereas the "Cancel" button has the cancelSignOut() button.

---
+++ src/components/modal-sign-out.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react'
+import { LoginContext } from './login-context'
+import { useNavigate } from 'react-router-dom'
+
+export default function SignOutModal({setModal}) {
+    const { setSignedIn } = useContext(LoginContext)
+    const navigate = useNavigate()
+
+    function cancelSignOut() {
+        setModal(false)
+    }
+
+    function signOut() {
+        setSignedIn(false)
+        navigate('/')
+    }
+    return (
+        <div className='modal-overlay'>
+            <div className='modal-window'>
+                <div className='modal-title'>
+                    <p>Sign Out Confirmation</p>
+                </div>
+                <div className='modal-body'>
+                    <p>Are you sure you want to sign out now?</p>
+                </div>
+                <div className='modal-bottom'>
+                    <button className='modal-buttons' onClick={signOut}>Yes</button>
+                    <button className='modal-buttons' onClick={cancelSignOut}>Cancel</button>
+                </div>
+            </div>
+        </div>
+      )
+}
