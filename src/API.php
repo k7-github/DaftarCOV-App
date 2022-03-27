1e8bed9786fc83a1011c7916ebc6a7feeab0cf9a
k7-github <100348682+k7-github@users.noreply.github.com>
2022-03-28 00:17:32 +0800

    Update API.php
    Defining the variables for the SQL connection variable.

---
+++ src/API.php
@@ -0,0 +1,8 @@
+<?php
+    $host = 'localhost';
+    $user = 'Kesavan';
+    $password = '';
+    $database = 'daftar-cov';
+    $conn = mysqli_connect($host, $user, $password, $database);
+    
+?>
