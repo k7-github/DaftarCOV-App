dfa34193188cafc19fbaa49d8e56062eb0df61df
k7-github <100348682+k7-github@users.noreply.github.com>
2022-03-28 18:12:12 +0800

    Moved to a New Location.
    New location: C:\xampp\htdocs\daftar-cov
    
    I just realised that this file will not function outside of the xampp folder.

--- src/API.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $host = 'localhost';
-    $user = 'Kesavan';
-    $password = '';
-    $database = 'daftar-cov';
-    $conn = mysqli_connect($host, $user, $password, $database);
-    
-?>
