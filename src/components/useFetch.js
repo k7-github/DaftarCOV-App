cf5fb05ae07505f765e7d71a957c0d8743c839fb
k7-github <100348682+k7-github@users.noreply.github.com>
2022-04-04 23:35:50 +0800

    Update useFetch.js

--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 
-export default function useFetch() {
-  
-  
-    return (
-    <div></div>
-  )
+export default function useFetch(url) {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const abortCont = new AbortController  
+    fetch(url, abortCont.signal)
+    .then (res => {
+        if (!res.ok) {
+            throw Error ("Data from the DaftarCOV database cannot be fetched.")
+        }
+        return res.json()
+    })
+    .then (data => {
+        setLoading(false)
+        setData(data)
+        setError(null)
+    })
+    .catch(err => {
+        if (err.name === 'AbortError') {
+            console.log('fetch aborted')
+        } else {
+            setLoading(false)
+            setError(err.message)
+        }
+
+        return { data, loading, error }
+    })
+  })
 }
